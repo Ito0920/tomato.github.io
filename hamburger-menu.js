69bdd7b2caeee435922b131f58009fdfb3920156
Ito0920 <87754361+Ito0920@users.noreply.github.com>
2021-08-14 15:25:48 +0900

    Add files via upload

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -2,31 +2,25 @@ document.addEventListener("DOMContentLoaded", function(){
   document.getElementById("menuButton").addEventListener("click", function() {
       this.classList.toggle("active");
       document.getElementById("nav").classList.toggle("active");
-      document.getElementById("body").classList.toggle("active");
   })
   document.getElementById("hambuger-menu").addEventListener("click", function() {
     document.getElementById("menuButton").classList.toggle("active");
     document.getElementById("nav").classList.toggle("active");
-    document.getElementById("body").classList.toggle("active");
   })
   document.getElementById("hambuger-menu2").addEventListener("click", function() {
     document.getElementById("menuButton").classList.toggle("active");
     document.getElementById("nav").classList.toggle("active");
-    document.getElementById("body").classList.toggle("active");
   })
   document.getElementById("hambuger-menu3").addEventListener("click", function() {
     document.getElementById("menuButton").classList.toggle("active");
     document.getElementById("nav").classList.toggle("active");
-    document.getElementById("body").classList.toggle("active");
   })
   document.getElementById("hambuger-menu4").addEventListener("click", function() {
     document.getElementById("menuButton").classList.toggle("active");
     document.getElementById("nav").classList.toggle("active");
-    document.getElementById("body").classList.toggle("active");
   })
   document.getElementById("hambuger-menu5").addEventListener("click", function() {
     document.getElementById("menuButton").classList.toggle("active");
     document.getElementById("nav").classList.toggle("active");
-    document.getElementById("body").classList.toggle("active");
   })
 });
