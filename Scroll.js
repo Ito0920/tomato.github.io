9e19000540ad16d39397bbde8dfeac4eea98282a
Ito0920 <87754361+Ito0920@users.noreply.github.com>
2021-08-15 04:58:50 +0900

    Add files via upload

--- Scroll.js
+++ Scroll.js
@@ -9,8 +9,9 @@ document.addEventListener("scroll", function(){
 })
 
 window.addEventListener('DOMContentLoaded', () => {
-    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+    const anchorLinks = document.querySelectorAll('a[href^="#"]')
     const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+    const header = document.querySelector('#header');
   
     anchorLinksArr.forEach(link => {
       link.addEventListener('click', e => {
@@ -18,8 +19,10 @@ window.addEventListener('DOMContentLoaded', () => {
         const targetId = link.hash;
         const targetElement = document.querySelector(targetId);
         const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+        const headerHeight = header.offsetHeight;
+        const totalScrollAmount = targetOffsetTop - headerHeight;
         window.scrollTo({
-          top: targetOffsetTop,
+          top: totalScrollAmount,
           behavior: "smooth"
         });
       });
